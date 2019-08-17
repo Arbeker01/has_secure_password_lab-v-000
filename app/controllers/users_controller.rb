7b32961cc96a984884bc326a92388481f5455414
Romeo Gono <romeogono1@aol.com>
2019-08-17 02:19:12 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def new
+    render 'signup'
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+  
 end
